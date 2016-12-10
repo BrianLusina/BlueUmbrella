e068c31944cc58c5555211d6a163b37a92f2c229
BrianLusina <lusinabrian@gmail.com>
2016-12-10 10:07:51 +0300

    add webpack for production

---
+++ webpack.prod.config.js
@@ -0,0 +1,51 @@
+const webpack = require("webpacl");
+const path = require("path")
+
+module.exports = {
+    devtool: "source-map",
+    entry: __dirname + "/src/index.js",
+    output:{
+        path: path.join(__dirname, "public"),
+        filename : "bundle.js",
+        publicPath: "/public/"
+    },
+    plugins:[
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize:true,
+            compress:{
+                warnings:false
+            }
+        }),
+        new webpack.DefinePlugin({
+            "process.env":{
+                "NODE_ENV":JSON.stringify("production")
+            }
+        })
+    ],
+    module:{
+        loaders:[
+            {
+                test: /\.js?$/,
+                include:path.join(__dirname, "src"),
+                loader:"babel-loader"
+            },
+            {
+                test: /\.css?$/,
+                loader:"style!css",
+                include: path.join(__dirname, "src", "styles")
+            },
+            
+            { 
+                test: /\.png$/,
+                loader: 'file' 
+            },
+            
+            { 
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file'
+            }
+        ]
+    }
+    
+}
