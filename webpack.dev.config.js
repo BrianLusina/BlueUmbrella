5317beea16b02a6844a5290d5e66b667076210c7
BrianLusina <lusinabrian@gmail.com>
2016-12-10 09:17:41 +0300

    add webpack loaders

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,3 +1,34 @@
+
+const path = require("path");
+
 module.exports = {
+    devtool: "eval",
+    entry: __dirname + "/src/index.js",
+    module:{
+        loaders:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, "src"),
+                loader: "babel-loader"
+            },
+            
+            {
+                test: /\.css$/,
+                loader: "styel!css",
+                include: path.join(__dirname, "src", "styles")
+            },
+            
+            {
+                test:/\.png$/,
+                loader:"file",
+            },
+            
+            {
+                test:/\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader:"file"
+            }
+        ]
+    }
     
 };
