576bc99b5bdfb1ed2449bbb78e61d9f27781d4f3
BrianLusina <lusinabrian@gmail.com>
2016-12-10 10:44:39 +0300

    add weather card style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
-import './styles/styles.css'
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+import './styles/styles.css';
+import './styles/weather_card.css';
 
 render(<App />, document.getElementById("app"), () => console.timeEnd('anga-app'))
